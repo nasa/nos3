4a9d9f2a42d82d732fccc995b74660fe602d4c95
Lucas, John P <John.P.Lucas@nasa.gov>
2019-02-08 11:01:49 -0500

    Fixed COSMOS install and verified using Vagrant 2.2.2 and Virtual Box 5.2.22;

--- support/installers/ubuntu/nos3_ubuntu_64_FULL_cosmos.sh
+++ support/installers/ubuntu/nos3_ubuntu_64_FULL_cosmos.sh
@@ -37,9 +37,45 @@ echo " "
     make 1> /dev/null
     make install 1> /dev/null
     ruby -v
-    # Install COSMOS gem
-    gem install cosmos 1> /dev/null
-    gem install bundler 1> /dev/null
+
+# Set ownership
+chown -R $NOS3_USER:root /usr/local/bin
+chown -R $NOS3_USER:root /var/lib/gems
+
+echo "Gem installation..."
+cd /home/$NOS3_USER
+sudo su $NOS3_USER << `EOF`
+    export NOS3_USER=$(whoami)
+
+    # Create necessary directories
+    test -e /home/$NOS3_USER/Desktop || mkdir /home/$NOS3_USER/Desktop
+
+    # Install gems
+    echo 'gem: --no-ri --no-rdoc' >> /home/$NOS3_USER/.gemrc
+    gem install ruby-termios -v 0.9.6 1> /dev/null
+    gem install rake -v 12.3.2 1> /dev/null
+    gem install bundler -v 1.3.0 1> /dev/null
+    gem install collection-json -v 0.1.7 1> /dev/null
+    gem install multipart-post -v 2.0.0 1> /dev/null
+    gem install faraday -v 0.15.4 1> /dev/null
+    gem install faraday_middleware -v 0.13.1 1> /dev/null
+    gem install faraday_collection_json -v 0.0.1 1> /dev/null
+    gem install middleware -v 0.1.0 1> /dev/null
+    gem install rack -v 2.0.6 1> /dev/null
+    gem install rack-cache -v 1.8.0 1> /dev/null
+    gem install ffi -v 1.10.0 1> /dev/null
+    gem install ethon -v 0.12.0 1> /dev/null
+    gem install typhoeus -v 1.3.1 1> /dev/null
+    gem install cosmos -v 4.3.0  1> /dev/null
+
+    echo "COSMOS installation..."
+    cd /home/$NOS3_USER/Desktop
+    test -e cosmos || cosmos install cosmos 1> /dev/null
+    cd /home/$NOS3_USER/Desktop/cosmos
+    cd /home/$NOS3_USER/nos3
+    cp -R /home/$NOS3_USER/nos3/support/cosmos/* /home/$NOS3_USER/Desktop/cosmos/
+    find /home/$NOS3_USER/Desktop/cosmos -type f | xargs dos2unix -q
+`EOF`
 
 echo " "
 echo "Setup Environment"
