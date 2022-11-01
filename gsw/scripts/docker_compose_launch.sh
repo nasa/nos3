d4eae619bfe22b9b69bb2febb74df7aa32e588c7
Mark Suder <mark.d.suder@nasa.gov>
2022-11-01 11:19:20 -0400

    https://github.com/nasa/nos3/issues/78 - Add COSMOS to docker compose.

--- gsw/scripts/docker_compose_launch.sh
+++ gsw/scripts/docker_compose_launch.sh
@@ -30,17 +30,14 @@ rm -rf NOS3InOut
 cp -r $BASE_DIR/sims/cfg/InOut /opt/nos3/42/NOS3InOut
 xhost +local:*
 
-cd $SCRIPT_DIR
-docker compose up -d
-
 echo "COSMOS Ground Station..."
 cd $BASE_DIR/gsw/cosmos
 export MISSION_NAME=$(echo "NOS3")
 export PROCESSOR_ENDIANNESS=$(echo "LITTLE_ENDIAN")
-docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e QT_X11_NO_MITSHM=1 \
-    -v /home/nos3/Desktop/github-nos3/gsw/cosmos:/cosmos/cosmos \
-    -v /home/nos3/Desktop/github-nos3/components/:/COMPONENTS -w /cosmos/cosmos -d --network=host \
-    ballaerospace/cosmos /bin/bash -c 'ruby Launcher -c nos3_launcher.txt --system nos3_system.txt && true' # true is necessary to avoid setpgrp error
+#docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e QT_X11_NO_MITSHM=1 \
+#    -v /home/nos3/Desktop/github-nos3/gsw/cosmos:/cosmos/cosmos \
+#    -v /home/nos3/Desktop/github-nos3/components/:/COMPONENTS -w /cosmos/cosmos -d --network=host \
+#    ballaerospace/cosmos /bin/bash -c 'ruby Launcher -c nos3_launcher.txt --system nos3_system.txt && true' # true is necessary to avoid setpgrp error
 
-sleep 5
-
+cd $SCRIPT_DIR
+docker compose up -d
