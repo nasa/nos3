fd4ca6d99189ac0636e402f41bd45f72afbc6cf6
Matt Grubb <matthew.d.grubb@nasa.gov>
2022-11-14 13:34:37 -0500

    Streamline RW momentum tlm reporting

--- fsw/nos3_defs/tables/sch_def_msgtbl.c
+++ fsw/nos3_defs/tables/sch_def_msgtbl.c
@@ -188,7 +188,7 @@ SCH_MessageEntry_t SCH_DefaultMessageTable[SCH_MAX_MESSAGES] =
     /* command ID #44 */
   { { CFE_MAKE_BIG16(SCH_UNUSED_MID) } },  
     /* command ID #45 - RW HK */
-  { { CFE_MAKE_BIG16(GENERIC_RW_APP_SEND_HK_MID),  CFE_MAKE_BIG16(0xC000), CFE_MAKE_BIG16(0x0001), 0x0000 } },
+  { { CFE_MAKE_BIG16(GENERIC_RW_APP_CMD_MID),  CFE_MAKE_BIG16(0xC000), CFE_MAKE_BIG16(0x0001), CFE_MAKE_BIG16(0x0200) } },
     /* command ID #46 */
   { { CFE_MAKE_BIG16(SCH_UNUSED_MID) } },
     /* command ID #47 */
