b8c9dbe064c9a322a345752e4f49591b9181980c
Lucas, John P <John.P.Lucas@nasa.gov>
2023-02-15 13:26:32 -0500

    [nasa/nos3#121] Initial configuration for UDP TF in FSW;

--- fsw/nos3_defs/toolchain-cpu1.cmake
+++ fsw/nos3_defs/toolchain-cpu1.cmake
@@ -36,5 +36,5 @@ SET(CMAKE_C_FLAGS_INIT "-m32" CACHE STRING "C Flags required by platform")
 add_definitions(-DBYTE_ORDER_LE)
 add_definitions(-D_LINUX_OS_)
 
-set(CI_TRANSPORT udp)
-set(TO_TRANSPORT udp)
+set(CI_TRANSPORT udp_tf)
+set(TO_TRANSPORT udp_tf)
