6fa3a726c7819ffc781ae781c77328e5ac9ff302
Mark Suder <mark.d.suder@nasa.gov>
2023-04-05 13:19:32 -0400

    https://github.com/nasa/nos3/issues/146 - Update hwlib and C FLAGS for CPU2 (pc-linux).  These flags allow FSW to build (does not build 32 bit due to lack of 32 bit libsocketcan.so)... runs ok under GDB, but dumps when not under GDB... none of the busses/devices exist, so not surprising.

--- fsw/nos3_defs/toolchain-cpu2.cmake
+++ fsw/nos3_defs/toolchain-cpu2.cmake
@@ -29,7 +29,8 @@ SET(OSAL_SYSTEM_BSPNAME     "pc-linux")
 SET(OSAL_SYSTEM_OSTYPE      "posix")
 
 # This adds the "-m32" flag to all compile commands
-SET(CMAKE_C_FLAGS_INIT "-m32" CACHE STRING "C Flags required by platform")
+#SET(CMAKE_C_FLAGS_INIT "-m32" CACHE STRING "C Flags required by platform")
+SET(CMAKE_C_FLAGS_INIT "-fPIC" CACHE STRING "C Flags required by platform")
 #SET(CMAKE_SHARED_LINKER_FLAGS "-pg")
 
 # Build Specific
