b67b81f004c640d1af37d82f8e977be5882257ac
Lucas, John P <John.P.Lucas@nasa.gov>
2023-06-13 15:03:08 -0400

    [nasa/nos3#115] Updated various submodules after PRs and added ADCS HK request to SCH table;

--- fsw/nos3_defs/tables/sch_def_schtbl.c
+++ fsw/nos3_defs/tables/sch_def_schtbl.c
@@ -129,7 +129,7 @@ SCH_ScheduleEntry_t SCH_DefaultScheduleTable[SCH_TABLE_ENTRIES] =
   /* slot #5  - Component HK */
 /*{  SCH_DISABLED, SCH_ACTIVITY_SEND_MSG,  1,  0, 24, SCH_GROUP_MD_WAKEUP }, */  /* MD Wakeup */
   {  SCH_ENABLED,  SCH_ACTIVITY_SEND_MSG,  1,  0, 40, SCH_GROUP_NONE },   /* CAM HK Request */                                      
-  {  SCH_UNUSED,   0,      0,  0, 0,  SCH_GROUP_NONE},                            
+  {  SCH_ENABLED,  SCH_ACTIVITY_SEND_MSG,  1,  0, 96, SCH_GROUP_NONE },   /* ADCS HK Request */       
   {  SCH_UNUSED,   0,      0,  0, 0,  SCH_GROUP_NONE},                                        
   {  SCH_UNUSED,   0,      0,  0, 0,  SCH_GROUP_NONE},                                        
   {  SCH_UNUSED,   0,      0,  0, 0,  SCH_GROUP_NONE},                                        
