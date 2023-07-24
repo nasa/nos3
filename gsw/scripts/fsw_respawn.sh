fdd81af537722035c88787af73d14b74040d2755
Lucas, John P <John.P.Lucas@nasa.gov>
2023-07-24 08:10:35 -0400

    [nasa/nos3#205] Update sample submodule and add additional check to fsw_respawn script to avoid multiple instances being launched;

--- gsw/scripts/fsw_respawn.sh
+++ gsw/scripts/fsw_respawn.sh
@@ -12,7 +12,12 @@ do
     pidof core-cpu1 > /dev/null
     if [ $? -eq 1 ]
     then
-        gnome-terminal --title="NOS3 Flight Software" -- $FSW_BIN/core-cpu1 -R PO
+        sleep 5
+        pidof core-cpu1 > /dev/null
+        if [ $? -eq 1 ]
+        then
+            gnome-terminal --title="NOS3 Flight Software" -- $FSW_BIN/core-cpu1 -R PO
+        fi
     fi
     sleep 1
 done
