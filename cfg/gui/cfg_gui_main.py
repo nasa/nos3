8263e9bc7db72939f620301375aef57ff8fe65c3
Ice <dice@ndc.nasa.gov>
2024-04-11 10:56:53 -0400

    QOL and Bug Fixes

--- cfg/gui/cfg_gui_main.py
+++ cfg/gui/cfg_gui_main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from PySide6.QtWidgets import QWidget, QApplication, QFileDialog, QTextEdit, QPushButton, QDateTimeEdit, QLabel, QCheckBox, QVBoxLayout, QSizePolicy, QDoubleSpinBox, QLayout
+from PySide6.QtWidgets import QWidget, QApplication, QFileDialog, QTextEdit, QPushButton, QDateTimeEdit, QLabel, QCheckBox, QVBoxLayout, QSizePolicy, QDoubleSpinBox, QLayout, QMessageBox
 from PySide6.QtCore import QProcess, QDateTime
 from PySide6.QtGui import QTextCharFormat
 
@@ -7,6 +7,10 @@ import sys, re, xmltodict, datetime, threading
 import xml.etree.ElementTree as ET
 
 from cfg_gui_ui import Ui_Form
+
+
+# TODO: Update master xml sc-x-cfg filename if modified in sc config (in progress)
+
 
 class cfg_gui(QWidget):
     def __init__(self, *args, **kwargs):
@@ -17,8 +21,9 @@ class cfg_gui(QWidget):
         self.setFixedSize(655, 655)
         self.setWindowTitle("NOS3 Igniter - Version 0.0.1")
 
+        # globals
         self.dateTimeEdit = QDateTimeEdit()
-        self.scConfigs = {}                                                     # Stores child configs {'filename' : "filetext"}
+        self.scConfigs = {}                                                     # Stores child configs {'index' : "filetext"}
         self.prevButtonPressed = None                                           # Tracks the last button pressed, used in buttonColor()
         self.defaultStyleSheet = self.ui.pushButton_buildAll.styleSheet()       # Saves default stylesheet to return button color to normal, buttonColor()
         self.setup = 0                                                          # Allows for switchConfig() to initially be called without calling saveText()
@@ -64,22 +69,32 @@ class cfg_gui(QWidget):
             self.dateTimeEdit.setMinimumDateTime(QDateTime(self.currentTime.year, self.currentTime.month, self.currentTime.day, self.currentTime.hour, self.currentTime.minute, self.currentTime.second, 0, 0))
             self.ui.horizontalLayout_runForUntil.insertWidget(1, self.dateTimeEdit)
             
-    # Overwrites the currently saved text for the currently selected spacecraft config when edited
+    # Updates the currently saved xml dictionary (not actual xml file) for the currently selected spacecraft config when edited. TODO: change name of function
     def saveText(self, layout:QLayout, config_value:int):
-        child = self.scConfigs[config_value]
-        text = child
+        text = self.scConfigs[config_value]
         filename = text.split('\n')[0]
-        childXml = text.split('\n', 2)[2]
-        childXml = xmltodict.parse(childXml)
+        childXml = xmltodict.parse(text.split('\n', 2)[2])
 
+        # TODO: change to dynamically pull apps/components from xml file or directory
         applications = ['cf', 'ds', 'fm', 'lc', 'sc']
         components = ['adcs', 'cam', 'css', 'eps', 'fss', 'gps', 'imu', 'mag', 'radio', 'rw', 'sample', 'st', 'torquer']
 
         i = 0
         while layout.itemAt(i) != None:
             widget = layout.itemAt(i).widget()
-            if isinstance(widget, QCheckBox):
-                #print(child.text())
+
+            # Handle filename
+            if isinstance(widget, QTextEdit):
+                widget:QTextEdit
+                filename = widget.toPlainText()
+                if "Filename:" not in filename:
+                    filename = f'Filename: {filename}'
+                if "Filename: " not in filename:
+                    filename = f'Filename: {filename.split(":")[1]}'
+
+            # Handle checkboxes
+            elif isinstance(widget, QCheckBox):
+                widget:QCheckBox
                 text = widget.text().split(' ')[0]
                 if text in applications:
                     childXml['sc-1-config']['applications'][text]['enable'] = str(widget.isChecked()).lower()
@@ -87,10 +102,14 @@ class cfg_gui(QWidget):
                     childXml['sc-1-config']['components'][text]['enable'] = str(widget.isChecked()).lower()
                 elif text == 'gui':
                     childXml['sc-1-config'][text]['enable'] = str(widget.isChecked()).lower()
+
+            # Handle orbits
             elif isinstance(widget, QDoubleSpinBox):
-                #print(child.prefix())
+                widget:QDoubleSpinBox
                 prefix = widget.prefix().split(' ')[0]
                 childXml['sc-1-config']['orbit'][prefix] = str(widget.value())
+
+            # Increment index
             i += 1
             
         combined = filename + '\n\n' + xmltodict.unparse(childXml)
@@ -101,7 +120,7 @@ class cfg_gui(QWidget):
         # saveType = "save" (overwrite) or "saveAs" (new)
 
         if saveType == "saveAs":
-            savePath, _ = QFileDialog.getSaveFileName(None, 'Directory', './cfg/custom')
+            savePath, _ = QFileDialog.getSaveFileName(None, 'Directory', './cfg')
         elif saveType == "save":
             savePath = self.config_path
 
@@ -109,7 +128,7 @@ class cfg_gui(QWidget):
         masterXml = xmltodict.parse(self.ui.textEdit_masterConfig.toPlainText())
         self.convert2xml(masterXml, savePath)
 
-        # TODO: Now handle children, currently placeholder until checkboxes are added (update in progress)
+        # Now handle children
         self.saveText(self.layout_, self.ui.spinBox_configNumber.value()-1)
 
         for child in self.scConfigs:
@@ -121,6 +140,8 @@ class cfg_gui(QWidget):
             # save under same directory as masterXml using filename parsed from textEdit
             self.convert2xml(childXml, savePath.rsplit('/', 1)[0]+f'/{filename}')
 
+        self.reloadConfig(self.config_path)
+
     # Loads the child config into the Spacecraft Config textbox
     def switchConfig(self, value:int):
         # value : index of spacecraft config in the order listed in the master XML
@@ -130,8 +151,10 @@ class cfg_gui(QWidget):
         if self.setup == 1:
             self.saveText(self.layout_, self.configNumTrack)
             self.configNumTrack = value-1
+        else:
+            self.setup = 1
 
-        self.setup = 1
+        # setup layout to add item to
         self.ui.scrollArea.setWidgetResizable(True)
         self.ui.scrollAreaWidgetContents.setLayout(QVBoxLayout().layout())
         self.layout_ = self.ui.scrollAreaWidgetContents.layout()
@@ -141,21 +164,22 @@ class cfg_gui(QWidget):
         while self.layout_.itemAt(0) != None:
             child = self.layout_.itemAt(0).widget().setParent(None)
 
-
+        # Now parse the xml and convert to widgets
         value = value-1
         if value in self.scConfigs:
             fileName = self.scConfigs[value].split('\n')[0]
             childXML = self.scConfigs[value].split('\n')[2::]
             childXML = ''.join(childXML)
             childDict = xmltodict.parse(childXML)
-            
-            policy = self.ui.scrollAreaWidgetContents.sizePolicy()
 
+            # child = sc-xxx-cfg
             for child in childDict:
-                configTag = QLabel()
+                configTag = QTextEdit()
                 configTag.setText((fileName))
-                configTag.setMinimumHeight(18)
+                configTag.setMinimumHeight(30)
                 self.layout_.addWidget(configTag)
+
+                # child2 = applications | components | gui | orbit
                 for child2 in childDict[child]:
                     tag = QLabel()
                     tag.setText(child2.upper()+": ")
@@ -164,7 +188,10 @@ class cfg_gui(QWidget):
                     tag.setFont(format.font())
                     tag.setMinimumHeight(18)
                     self.layout_.addWidget(tag)
+
                     if child2 in ['applications', 'components']:
+
+                        # child 3 = cf | ds | ... | adcs | cam | ...
                         for child3 in childDict[child][child2]:
                             enableTag = QCheckBox()
                             enableTag.setText(child3 + " enable ")
@@ -172,12 +199,16 @@ class cfg_gui(QWidget):
                             enableTag.setMinimumHeight(18)
                             enableTag.sizePolicy().setVerticalPolicy(QSizePolicy.Expanding)
                             self.layout_.addWidget(enableTag)
+
                     elif child2 == 'gui':
                         enableTag = QCheckBox()
                         enableTag.setText(child2 + " enable ")
                         enableTag.setChecked(childDict[child][child2]['enable'] == 'true')
                         self.layout_.addWidget(enableTag)
+
                     elif child2 == 'orbit':
+
+                        # child3 = tipoff_x/y/z
                         for child3 in childDict[child][child2]:
                             orbitSpinBox = QDoubleSpinBox()
                             orbitSpinBox.setMinimum(-99.00)
@@ -186,12 +217,13 @@ class cfg_gui(QWidget):
                             orbitSpinBox.setPrefix(f'{child3} = ')
                             self.layout_.addWidget(orbitSpinBox)
         else:
+            # No SC configs in master XML file, or selected a SC XML as master
             tag = QLabel()
             tag.setText("*ERROR*\n\nMake sure you chose a master configuration file\n\n*ERROR*")
             self.layout_.addWidget(tag)
 
     # Converts a dictionary to XML file, saved under the given filename/path
-    def convert2xml(self, attrDict:dict, fileName:[str, Path]):
+    def convert2xml(self, attrDict:dict, fileName:str):
         # ensure file is saved as xml
         if fileName[-4::] != ".xml":
             fileName += ".xml"
@@ -203,39 +235,63 @@ class cfg_gui(QWidget):
 
     # Opens file selection menu and calls parseXML() on the selected file
     def browseConfig(self):
+
+        # Clear SC Config window when selecting a new master config
+        if "layout_" in self.__dict__:
+            while self.layout_.itemAt(0) != None:
+                self.layout_.itemAt(0).widget().setParent(None)
+
         self.config_path, _ = QFileDialog.getOpenFileName(None, 'File', './cfg', "XML Files [ *.xml ]")
         if self.config_path != "":
             self.config_name = self.config_path.split("/")[-1]
             self.ui.lineEdit_curConfig.setText(self.config_name)
             self.parseXml(self.config_path)
 
+    # Reloads the whole config after clicking save, allows you to change "sc-x-cfg" xml file
+    def reloadConfig(self, config_path):
+        if "layout_" in self.__dict__:
+            while self.layout_.itemAt(0) != None:
+                self.layout_.itemAt(0).widget().setParent(None)
+        
+        if config_path != "":
+            config_name = config_path.split("/")[-1]
+            self.ui.lineEdit_curConfig.setText(config_name)
+            self.parseXml(config_path)
+
+        self.ui.spinBox_configNumber.setValue(1)
+
     # Parses Master and child XML files from the given file, updates text boxes accordingly
     def parseXml(self, config_path):
         
-        # Master
+        # Read Master
         with open(config_path, 'r') as f:
             self.ui.textEdit_masterConfig.setText(f.read())
             f.close()
 
+        # Parse number of SC and SC filenames from master
         i = 1
+        self.sc_cfg_files = []
         childDict = {}
-        root = ET.parse(config_path).getroot()
-        for child in root:
+        self.master_root = ET.parse(config_path).getroot()
+        for child in self.master_root:
             if child.tag == "number-spacecraft":
                  self.ui.spinBox_configNumber.setMaximum(int(child.text))
             if re.match("sc-[0-9]+-cfg", child.tag):
                  childDict[child.tag] = child.text
+
+                 # Check for duplicate xml's
+                 if child.text in self.sc_cfg_files:
+                     QMessageBox.critical(self, "Error", "Using duplicate Spacecraft Config Files, Changes will not be saved correctly")
+                     print("Duplicate SC Config file")
+
+                 self.sc_cfg_files.append(child.text)
                  i+=1
 
-        # Children
+        # Read Children
         config_dir = str(config_path.rsplit('/', 1)[0])
         for i, child in enumerate(childDict):
             if Path(f'{config_dir}/{childDict[child]}').is_file():
                 filePath = f'{config_dir}/{childDict[child]}'
-            elif Path(f'./cfg/{childDict[child]}').is_file():
-                filePath = f'./cfg/{childDict[child]}'
-            elif Path(f'./cfg/custom/{childDict[child]}').is_file():
-                filePath = f'./cfg/custom/{childDict[child]}'
             else:
                 raise FileNotFoundError(childDict[child])
 
@@ -243,7 +299,7 @@ class cfg_gui(QWidget):
                 self.scConfigs[i] = f'Filename: {childDict[child]}\n\n{f.read()}'
                 f.close()
                 
-        # Update Spacecraft Config Text
+        # Update Spacecraft Config Text to first SC config listed in master config
         self.switchConfig(1)
 
     # Starts a Bash process to execute args, redirects output to given textbox, not used for now
@@ -285,7 +341,10 @@ class cfg_gui(QWidget):
     # Placeholder build command, assumes make prep already ran, same with clean commands
     def build(self, software:str, button:QPushButton):
         textbox = self.ui.textEdit_buildConsole
-        command = f'make {software}'
+        if software == 'all':
+            command = f'make'
+        else:
+            command = f'make {software}'
 
         self.buttonColor(button)
         t1 = threading.Thread(target=self.thread_Bash(textbox, button, command), name='t1')
@@ -297,7 +356,7 @@ class cfg_gui(QWidget):
         self.gnome_terminal(textbox, command)
         self.enableButtons(button)
 
-    # Changes the color of the most recently pressed button to green
+    # Changes the color of the most recently pressed button to green and the last pressed button to default
     def buttonColor(self, button:QPushButton):
         if self.prevButtonPressed is not None:
             self.prevButtonPressed.setStyleSheet(self.defaultStyleSheet)
