a8c41b5ca2a3b4ede581981c1a68ddf04bc824d9
Zachary Lynch <zachary.j.lynch@nasa.gov>
2024-05-02 19:55:32 +0000

    [nasa/nos3#84] edits/additions for fprime organization for fsw/fprime structure

---
+++ scripts/fsw_fprime_launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -i
+#
+# Convenience script for NOS3 development
+# Use with the Dockerfile in the deployment repository
+# https://github.com/nasa-itc/deployment
+#
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/env.sh
+
+
+cd fsw/fprime/fprime-nos3/
+. fprime-venv/bin/activate
+fprime-util build
+fprime-gds --gui-port 5000 --gui-addr 0.0.0.0
