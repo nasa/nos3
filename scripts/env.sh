77bb328dd20aef76721e7cd235978d5118eb83f5
zlynch2 <zachary.j.lynch@nasa.gov>
2024-09-12 14:10:00 +0000

    clean up scripts, removed prep fprime because of docker build now

--- scripts/env.sh
+++ scripts/env.sh
@@ -10,7 +10,6 @@ GSW_BIN=$BASE_DIR/gsw/cosmos/build/openc3-cosmos-nos3
 GSW_DIR=$BASE_DIR/gsw/cosmos
 SIM_DIR=$BASE_DIR/sims/build
 SIM_BIN=$SIM_DIR/bin
-FLIGHT_SOFTWARE=fprime
 
 if [ -d $SIM_DIR/bin ]; then
     SIMS=$(ls $SIM_BIN/nos3*simulator) 
--- scripts/prep_fprime.sh
+++ scripts/prep_fprime.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd fsw/fprime/fprime-nos3
-mkdir -p fprime-venv
-python3 -m venv fprime-venv/
-. fprime-venv/bin/activate
-pip install -r fprime/requirements.txt
-fprime-util generate
+# cd fsw/fprime/fprime-nos3
+# # mkdir -p fprime-venv
+# # python3 -m venv fprime-venv/
+# # . fprime-venv/bin/activate
+# # pip install -r fprime/requirements.txt
+# fprime-util generate
+
+echo "Dockerized fprime util generate"
