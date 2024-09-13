3176c704cfd612b28ec539aad1eed1b50ba0189b
Kevin Bruce <kevin.c.bruce@nasa.gov>
2024-09-13 13:18:53 -0400

    [nasa/nos3#298] Added onair_launch.sh to sleep before launching OnAIR so it is launched after SBN is initialized, and added extra debug statements to the client initialization.

--- scripts/docker_launch.sh
+++ scripts/docker_launch.sh
@@ -91,9 +91,9 @@ do
     xhost +local:*
     gnome-terminal --tab --title=$SC_NUM" - 42" -- $DFLAGS -e DISPLAY=$DISPLAY -v $USER_NOS3_DIR:$USER_NOS3_DIR -v /tmp/.X11-unix:/tmp/.X11-unix:ro --name $SC_NUM"_fortytwo" -h fortytwo --network=$SC_NETNAME -w $USER_NOS3_DIR/42 -t $DBOX $USER_NOS3_DIR/42/42 NOS3InOut
     echo ""
-
+    
     echo $SC_NUM " - OnAIR..."
-    gnome-terminal --window-with-profile=KeepOpen --title=$SC_NUM" - OnAIR" -- $DFLAGS -v $BASE_DIR:$BASE_DIR --name $SC_NUM"_onair" --network=$SC_NETNAME -w $FSW_DIR -t $DBOX python3 cf/onair/driver.py cf/onair/cfs_sample.ini
+    gnome-terminal --window-with-profile=KeepOpen --title=$SC_NUM" - OnAIR" -- $DFLAGS -v $BASE_DIR:$BASE_DIR --name $SC_NUM"_onair" --network=$SC_NETNAME -w $FSW_DIR -t $DBOX $SCRIPT_DIR/onair_launch.sh
     echo ""
 
     echo $SC_NUM " - Flight Software..."
---
+++ scripts/onair_launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Script to start OnAIR
+#
+
+sleep 16
+
+python3 cf/onair/driver.py cf/onair/cfs_sample.ini
