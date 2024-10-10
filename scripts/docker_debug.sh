f9b9e60400cf0c69893b9a5328cb6bc41ce212b8
zlynch2 <zachary.j.lynch@nasa.gov>
2024-10-10 17:21:47 +0000

    [nasa/nos3#84] reverted changes to normal nos3 docker debug, making issue to test fprime docker debug

--- scripts/docker_debug.sh
+++ scripts/docker_debug.sh
@@ -9,9 +9,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/env.sh
 
 mkdir -p $BASE_DIR/fsw/build
-# $DFLAGS_CPUS -v $BASE_DIR:$BASE_DIR -v $USER_NOS3_DIR:$USER_NOS3_DIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
+$DFLAGS_CPUS -v $BASE_DIR:$BASE_DIR -v $USER_NOS3_DIR:$USER_NOS3_DIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
 # $DFLAGS_CPUS -it -v $BASE_DIR:$BASE_DIR --network nos3_sc_1 -p 5000:5000 -p 50000:50000 -p 5001:5001 -v $USERDIR:$USERDIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
-$DFLAGS_CPUS -it -v $BASE_DIR:$BASE_DIR  -p 5000:5000 -p 50000:50000 -p 5001:5001 -v $USERDIR:$USERDIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
-# $DFLAGS_CPUS -v $BASE_DIR:$BASE_DIR --network host -v $USERDIR:$USERDIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
-# $DFLAGS_CPUS -v $BASE_DIR:$BASE_DIR --expose 5000 -v $USERDIR:$USERDIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
-# $DFLAGS_CPUS -v $BASE_DIR:$BASE_DIR -p 5000:5000 -v $USERDIR:$USERDIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
+# $DFLAGS_CPUS -it -v $BASE_DIR:$BASE_DIR  -p 5000:5000 -p 50000:50000 -p 5001:5001 -v $USERDIR:$USERDIR -w $BASE_DIR --name "nos3_debug" $DBOX bash
