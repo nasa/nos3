ce87f529ba81f8b70d959875be0d46f6437a832f
Lucas, John P. <John.P.Lucas@nasa.gov>
2024-10-16 10:09:11 -0400

    [nasa/nos3#399] Updated ait-server to use $DCALL instead of all the $DFLAGS to avoid shared folder issues;

--- scripts/gsw_ait_launch.sh
+++ scripts/gsw_ait_launch.sh
@@ -9,7 +9,7 @@ source $SCRIPT_DIR/env.sh
 export GSW="ait"
 
 echo "AIT launch..."
-gnome-terminal --tab --title="AIT" -- $DFLAGS -v $BASE_DIR:$BASE_DIR -v /tmp/nos3:/tmp/nos3 --name ait -h ait -p 8001:8001 --network=nos3_core ghcr.io/sphinxdefense/gsw-ait:main "source ~/.bashrc && ait-server"
+gnome-terminal --tab --title="AIT" -- $DCALL run --rm -it -v $BASE_DIR:$BASE_DIR -v /tmp/nos3:/tmp/nos3 --name ait -h ait -p 8001:8001 --network=nos3_core ghcr.io/sphinxdefense/gsw-ait:main "source ~/.bashrc && ait-server"
 $DCALL run --rm -d --cpus=$NUM_CPUS -h influxdb --name influxdb -p 8086:8086 -e INFLUXDB_DB=$INFLUXDB_DB -e INFLUXDB_ADMIN_USER=$INFLUXDB_ADMIN_USER -e INFLUXDB_ADMIN_PASSWORD=$INFLUXDB_ADMIN_PASSWORD --network=nos3_core influxdb:1.8
 $DCALL run --rm -d --cpus=$NUM_CPUS --name ttc-command -p 80:80 --network=nos3_core ghcr.io/sphinxdefense/ttc-command:main
 echo ""
