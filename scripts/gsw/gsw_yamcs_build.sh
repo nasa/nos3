2f58b4f806ff965f7cd7019565c358e390e3771e
Lucas, John P. <John.P.Lucas@nasa.gov>
2024-11-14 08:31:02 -0500

    [nasa/nos3#80] Updates to use nos3 container, stop, and uninstall as expected;

--- scripts/gsw/gsw_yamcs_build.sh
+++ scripts/gsw/gsw_yamcs_build.sh
@@ -8,7 +8,6 @@ SCRIPT_DIR=$CFG_BUILD_DIR/../../scripts
 source $SCRIPT_DIR/env.sh
 
 echo "YAMCS build..."
-$DCALL image pull maven:3.9.9-eclipse-temurin-17
 rm -rf $USER_NOS3_DIR/yamcs 2> /dev/null
 cp -r $BASE_DIR/gsw/yamcs $USER_NOS3_DIR/
 echo ""
--- scripts/gsw/gsw_yamcs_launch.sh
+++ scripts/gsw/gsw_yamcs_launch.sh
@@ -8,10 +8,10 @@ SCRIPT_DIR=$CFG_BUILD_DIR/../../scripts
 source $SCRIPT_DIR/env.sh
 
 echo "YAMCS Launch... "
-gnome-terminal --tab --title="YAMCS" -- $DFLAGS -v $BASE_DIR:$BASE_DIR -v $USER_NOS3_DIR:$USER_NOS3_DIR -p 8090:8090 -p 5012:5012 --name cosmos_openc3-operator_1 -h cosmos --network=nos3_core --network-alias=cosmos -w $USER_NOS3_DIR/yamcs maven:3.9.9-eclipse-temurin-17 mvn -Dmaven.repo.local=$USER_NOS3_DIR/.m2/repository yamcs:run
+gnome-terminal --tab --title="YAMCS" -- $DFLAGS -v $BASE_DIR:$BASE_DIR -v $USER_NOS3_DIR:$USER_NOS3_DIR -p 8090:8090 -p 5012:5012 --name cosmos_openc3-operator_1 -h cosmos --network=nos3_core --network-alias=cosmos -w $USER_NOS3_DIR/yamcs $DBOX mvn -Dmaven.repo.local=$USER_NOS3_DIR/.m2/repository yamcs:run
 
 pidof firefox > /dev/null
 if [ $? -eq 1 ]
 then
-    firefox localhost:8090 &
+    sleep 20 && firefox localhost:8090 &
 fi
