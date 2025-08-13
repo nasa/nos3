e3a9820a3a154ed53db3efc45dce4572241decad
Haisam Ido <haisam.ido@nasa.gov>
2025-08-13 15:54:23 -0400

    updates

--- deployments/docker/sidecar/commanding.py
+++ deployments/docker/sidecar/commanding.py
@@ -3,8 +3,9 @@ from yamcs.client import YamcsClient
 client = YamcsClient('active-gs:8090')
 processor = client.get_processor(instance='nos3', processor='realtime')
 
-command_name = "/GENERIC_RADIO/CMD/GENERIC_RADIO_NOOP_CC"
-arguments = {"voltage_num": 1} # Example arguments, NOT RELEVANT FOR NOOP command
+command_name = "/CFS/CMD/TO_ENABLE_OUTPUT"
+arguments = {} # Example arguments, NOT RELEVANT FOR NOOP command
 
 command_handle = processor.issue_command(command_name, args=arguments)
+
 print(f"Issued command: {command_handle}")
--- deployments/docker/sidecar/entrypoint.sh
+++ deployments/docker/sidecar/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 
+curl -X POST http://active-gs:8090/api/links/nos3/radio-in:disable
 curl -X POST http://active-gs:8090/api/links/nos3/radio-out:disable
-sleep 1
-curl -X POST http://active-gs:8090/api/links/nos3/radio-out:enable
-
 curl -X POST http://active-gs:8090/api/links/nos3/truth42-in:disable
-sleep 1
+
+sleep 11
+
+curl -X POST http://active-gs:8090/api/links/nos3/radio-in:enable
+curl -X POST http://active-gs:8090/api/links/nos3/radio-out:enable
 curl -X POST http://active-gs:8090/api/links/nos3/truth42-in:enable
 
 pip install --upgrade yamcs-client && python3 /commanding.py
