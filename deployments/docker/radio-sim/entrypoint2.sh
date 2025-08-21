f5896ee63e7ed43e919b7c9f7ebdb77e42d9cbbc
Haisam Ido <haisam.ido@nasa.gov>
2025-08-21 12:40:38 -0400

    merged radio-sim & cryptolib into one container to stabilized instantiation of the services, since they rely on each other

---
+++ deployments/docker/radio-sim/entrypoint2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+cd /home/nos3/builds/nos3/sims/build/bin
+pkill -f ./nos3-single-simulator
+./nos3-single-simulator -f ./nos3-simulator.xml generic-radio-sim &
+
+pkill -f ./support/standalone
+./support/standalone &
+
+tail -f /dev/null
