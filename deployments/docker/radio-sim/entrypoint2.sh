e6ab2d2cdb5c0215b9e96675359695b2448ae92f
Haisam Ido <haisam.ido@nasa.gov>
2025-08-21 14:57:50 -0400

    fixed standalone path problem in entrypoint2.sh

--- deployments/docker/radio-sim/entrypoint2.sh
+++ deployments/docker/radio-sim/entrypoint2.sh
@@ -4,7 +4,10 @@ cd /home/nos3/builds/nos3/sims/build/bin
 pkill -f ./nos3-single-simulator
 ./nos3-single-simulator -f ./nos3-simulator.xml generic-radio-sim &
 
+cd /home/nos3/builds/nos3/gsw/build
 pkill -f ./support/standalone
 ./support/standalone &
+
+cd -
 
 tail -f /dev/null
