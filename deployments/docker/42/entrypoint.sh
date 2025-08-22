154e143aa8ab6796ef250829888760f846c2d10d
Haisam Ido <haisam.ido@nasa.gov>
2025-08-21 22:56:12 -0400

    added SERVER and PORT

--- deployments/docker/42/entrypoint.sh
+++ deployments/docker/42/entrypoint.sh
@@ -8,7 +8,7 @@ pkill -9 -f Xvnc || true
 pkill -9 -f websockify || true
 
 /opt/TurboVNC/bin/vncserver -securitytypes tlsnone,x509none,none && \
-  sleep 3 && \
+  sleep 5 && \
   websockify -D \
     --web=/usr/share/novnc/ \
     --cert=~/novnc.pem 80 localhost:5901 
