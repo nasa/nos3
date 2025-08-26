f6f633a0398622614f3f98846b84492a6b3e2134
Haisam Ido <haisam.ido@nasa.gov>
2025-08-26 14:50:55 +0000

    added sidecar.sh script

---
+++ deployments/docker/sidecar/sidecar.sh
@@ -0,0 +1,41 @@
+SERVER=$1
+SERVER=${SERVER:-localhost}
+
+PORT=$2
+PORT=${PORT:-8090}
+
+COMMAND=$3
+COMMAND=${COMMAND:-/CFS/CMD/TO_ENABLE_OUTPUT}
+INSTANCE=nos3
+
+curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-in:disable
+curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-out:disable
+curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/truth42-in:disable
+
+sleep 5
+
+curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-in:enable
+curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-out:enable
+curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/truth42-in:enable
+
+pip3 install --upgrade yamcs-client && \
+
+python3 <<EOF
+import sys
+
+from yamcs.client import YamcsClient
+
+SERVER='${SERVER}'
+PORT=${PORT}
+INSTANCE='${INSTANCE}'
+
+client = YamcsClient(f"https://{SERVER}:{PORT}",tls_verify=False)
+processor = client.get_processor(instance='${INSTANCE}', processor='realtime')
+
+command_name = "${COMMAND}"
+arguments = {} # Example arguments, NOT RELEVANT FOR NOOP command
+
+command_handle = processor.issue_command(command_name, args=arguments)
+
+print(f"Issued command: {command_handle}")
+EOF
