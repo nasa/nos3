bca651c3ba6c78595b7b56ef3363bd8cda649b92
Haisam Ido <haisam.ido@nasa.gov>
2025-08-27 16:26:29 +0000

    updates to sidecar.sh and permissions to .json files

--- deployments/docker/sidecar/sidecar.sh
+++ deployments/docker/sidecar/sidecar.sh
@@ -8,15 +8,17 @@ COMMAND=$3
 COMMAND=${COMMAND:-/CFS/CMD/TO_ENABLE_OUTPUT}
 INSTANCE=nos3
 
-curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-in:disable
-curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-out:disable
-curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/truth42-in:disable
+PROTOCOL=http
+
+curl -k -X POST ${PROTOCOL}://${SERVER}:${PORT}/api/links/nos3/radio-in:disable
+curl -k -X POST ${PROTOCOL}://${SERVER}:${PORT}/api/links/nos3/radio-out:disable
+curl -k -X POST ${PROTOCOL}://${SERVER}:${PORT}/api/links/nos3/truth42-in:disable
 
 sleep 5
 
-curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-in:enable
-curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/radio-out:enable
-curl -k -X POST https://${SERVER}:${PORT}/api/links/nos3/truth42-in:enable
+curl -k -X POST ${PROTOCOL}://${SERVER}:${PORT}/api/links/nos3/radio-in:enable
+curl -k -X POST ${PROTOCOL}://${SERVER}:${PORT}/api/links/nos3/radio-out:enable
+curl -k -X POST ${PROTOCOL}://${SERVER}:${PORT}/api/links/nos3/truth42-in:enable
 
 pip3 install --upgrade yamcs-client && \
 
@@ -28,11 +30,13 @@ from yamcs.client import YamcsClient
 SERVER='${SERVER}'
 PORT=${PORT}
 INSTANCE='${INSTANCE}'
+PROTOCOL='${PROTOCOL}'
+COMMAND='${COMMAND}'
 
-client = YamcsClient(f"https://{SERVER}:{PORT}",tls_verify=False)
-processor = client.get_processor(instance='${INSTANCE}', processor='realtime')
+client = YamcsClient(f"{PROTOCOL}://{SERVER}:{PORT}", tls_verify=False)
+processor = client.get_processor(instance=f"{INSTANCE}", processor='realtime')
 
-command_name = "${COMMAND}"
+command_name = f"{COMMAND}"
 arguments = {} # Example arguments, NOT RELEVANT FOR NOOP command
 
 command_handle = processor.issue_command(command_name, args=arguments)
