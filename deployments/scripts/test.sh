d0678238d9247de4aae5b7eba099e9fd3dc2618a
Haisam Ido <haisam.ido@nasa.gov>
2026-06-22 14:28:36 -0400

    updated test.sh

--- deployments/scripts/test.sh
+++ deployments/scripts/test.sh
@@ -12,43 +12,74 @@ task env:generate PROJECT=ssmo FLEET=ssmo MISSION=mms SPACECRAFT=mms3 FORTYTWO_H
 # Helm Chart stuff
 
 # Enviro variables if needed
-export K8S_CONTEXT=docker-desktop
+cd ./deployments
 
 # Build necessary images once
-task k8s:helm:build
+task build
 
-# Install Specific mission-spacecraft in parallel
-task k8s:helm:install K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms1 FORTYTWO_HOST_PORT=40091 YAMCS_HOST_PORT=18091 OPENMCT_HOST_PORT=19001 &
-task k8s:helm:install K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms2 FORTYTWO_HOST_PORT=40092 YAMCS_HOST_PORT=18092 OPENMCT_HOST_PORT=19002 &
-wait
+# Note the below varying SPACECRAFT and PORT values
 
-# Uninstall Specific mission-spacecraft in parallel
-task k8s:helm:delete K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms1 &
-task k8s:helm:delete K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms2 &
-wait
+#------------------------------------------------------------------------
+#   Docker via Docker Compose for a specific mission-spacecraft
+#------------------------------------------------------------------------
+task generate:env \
+  PROJECT=nos3 MISSION=m01 SPACECRAFT=sc01 \
+  FORTYTWO_HOST_PORT=10081 YAMCS_HOST_PORT=18091 OPENMCT_HOST_PORT=19001 && \
+  task up
 
-# Uninstall Helm charts
-task k8s:helm:delete:all K8S_CONTEXT=${K8S_CONTEXT} 
-wait
+task generate:env \
+  PROJECT=nos3 MISSION=m01 SPACECRAFT=sc02 \
+  FORTYTWO_HOST_PORT=10082 YAMCS_HOST_PORT=18092 OPENMCT_HOST_PORT=19002 && \
+  task up
+#------------------------------------------------------------------------
 
-# Port-forward all services:
-task k8s:helm:port-forward K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms1 FORTYTWO_HOST_PORT=40091 YAMCS_HOST_PORT=18091 OPENMCT_HOST_PORT=19001 &
-wait
+#------------------------------------------------------------------------
+#   Deploy NOS3 via K8S Helm Chart for a specific mission-spacecraft in parallel
+#------------------------------------------------------------------------
+export K8S_CONTEXT=docker-desktop
 
-task k8s:helm:port-forward K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms2 FORTYTWO_HOST_PORT=40092 YAMCS_HOST_PORT=18092 OPENMCT_HOST_PORT=19002 &
-wait
+task k8s:helm:up K8S_CONTEXT=${K8S_CONTEXT} \
+  PROJECT=nos3 MISSION=m01 SPACECRAFT=sc01 \
+  FORTYTWO_HOST_PORT=20081 YAMCS_HOST_PORT=28091 OPENMCT_HOST_PORT=29001 &
 
-# List all Port-forward services:
+task k8s:helm:up K8S_CONTEXT=${K8S_CONTEXT} \
+  PROJECT=nos3 MISSION=m01 SPACECRAFT=sc02 \
+  FORTYTWO_HOST_PORT=20082 YAMCS_HOST_PORT=28092 OPENMCT_HOST_PORT=29002 &
+#------------------------------------------------------------------------
+
+#------------------------------------------------------------------------
+# List all port-forward services in K8S_CONTEXT
+#------------------------------------------------------------------------
 task k8s:helm:port-forward:list K8S_CONTEXT=${K8S_CONTEXT}
 
-# Kill port-forward for specific services:
+#------------------------------------------------------------------------
+# Kill port-forward for specific spacecraft in K8S_CONTEXT
+#------------------------------------------------------------------------
 task k8s:helm:port-forward:kill K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms1 &
 task k8s:helm:port-forward:kill K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms2 &
 wait
 
-# Kill all port-forwards
-task k8s:helm:port-forward:kill:all K8S_CONTEXT=${K8S_CONTEXT} 
+#------------------------------------------------------------------------
+# Kill all port-forwards to K8S_CONTEXT
+#------------------------------------------------------------------------
+task k8s:helm:port-forward:kill:all K8S_CONTEXT=${K8S_CONTEXT}
 
+#------------------------------------------------------------------------
+# Delete specific mission-spacecraft in parallel in K8S_CONTEXT
+#------------------------------------------------------------------------
+task k8s:helm:delete K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms1 &
+task k8s:helm:delete K8S_CONTEXT=${K8S_CONTEXT} PROJECT=ssmo MISSION=mms SPACECRAFT=mms2 &
+wait
+
+#------------------------------------------------------------------------
+# Delete all Helm charts in K8S_CONTEXT
+#------------------------------------------------------------------------
+task k8s:helm:delete:all K8S_CONTEXT=${K8S_CONTEXT}
+
+#------------------------------------------------------------------------
+# Restore Environment to defaults in K8S_CONTEXT
+#------------------------------------------------------------------------
+task env:set:defaults
 
 exit 0
 
